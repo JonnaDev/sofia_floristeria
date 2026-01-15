f4737677974abc8447633513d01c1e56db28b382
JonnaDev <johnnlopezt@gmail.com>
2026-01-15 09:56:56 -0500

    Se implementa Update en Request de Categoria.

---
+++ app/Http/Requests/UpdateCategoryRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Category;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(Category $category): array
+    {
+        return 
+        [
+            //XD
+            'name' => 'required|string|max:255|unique:categories,id,' . $category->id
+        ];
+    }
+
+    public function messages(): array
+    {
+        return
+        [
+            'name.required' => 'El nombre de la categoría es obligatorio.',
+            'name.unique' => 'Esta categoría ya existe.',
+            'name.max' => 'El nombre no puede exceder 255 caracteres.',
+        ];
+    }
+}
