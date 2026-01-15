134639677b33f48e0d9943f64677a50e47d11bcb
JonnaDev <johnnlopezt@gmail.com>
2026-01-15 10:02:13 -0500

    Traducido al español y personalizar login default flux.

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.auth>
     <div class="flex flex-col gap-6">
-        <x-auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')" />
+        <x-auth-header :title="__('Inicia Sesion Para Acceder')" :description="__('Ingresa tu email y contraseña para ingresar')" />
 
         <!-- Session Status -->
         <x-auth-session-status class="text-center" :status="session('status')" />
@@ -17,7 +17,7 @@
                 required
                 autofocus
                 autocomplete="email"
-                placeholder="email@example.com"
+                placeholder="Tu correo electronico aquí"
             />
 
             <!-- Password -->
@@ -28,32 +28,30 @@
                     type="password"
                     required
                     autocomplete="current-password"
-                    :placeholder="__('Password')"
+                    :placeholder="__('Tu contraseña aquí')"
                     viewable
                 />
 
                 @if (Route::has('password.request'))
-                    <flux:link class="absolute top-0 text-sm end-0" :href="route('password.request')" wire:navigate>
-                        {{ __('Forgot your password?') }}
+                    <flux:link class="absolute top-0 text-sm end-0 text-pink-500" :href="route('password.request')" wire:navigate>
+                        {{ __('Olvidaste tu contraseña') }}
                     </flux:link>
                 @endif
             </div>
 
             <!-- Remember Me -->
-            <flux:checkbox name="remember" :label="__('Remember me')" :checked="old('remember')" />
+            <flux:checkbox name="remember" :label="__('Recordarme')" :checked="old('remember')" />
 
             <div class="flex items-center justify-end">
-                <flux:button variant="primary" type="submit" class="w-full" data-test="login-button">
-                    {{ __('Log in') }}
+                <flux:button variant="primary" type="submit" class="w-full" data-test="login-button" color="pink">
+                    {{ __('Iniciar Sesion') }}
                 </flux:button>
             </div>
         </form>
 
-        @if (Route::has('register'))
+
             <div class="space-x-1 text-sm text-center rtl:space-x-reverse text-zinc-600 dark:text-zinc-400">
-                <span>{{ __('Don\'t have an account?') }}</span>
-                <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
+                <flux:link wire:navigate>El sistema solo admite usuarios admin.</flux:link>
             </div>
-        @endif
     </div>
 </x-layouts.auth>
